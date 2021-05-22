e9d9ac04790cac6e08b68e6f0af1240f679bb0af
GUILLIN Yannick <yann4460@gmail.com>
2021-05-22 19:30:08 +0200

    Add json implem

--- src/Domain/Values/IBinaryType.cs
+++ src/Domain/Values/IBinaryType.cs
@@ -1,4 +1,7 @@
-﻿namespace Elrond.Dotnet.Sdk.Domain.Values
+﻿using System.Collections.Generic;
+using System.Text.Json;
+
+namespace Elrond.Dotnet.Sdk.Domain.Values
 {
     public interface IBinaryType
     {
@@ -7,6 +10,12 @@
         T ValueOf<T>() where T : IBinaryType
         {
             return (T) this;
+        }
+
+        string ToJSON()
+        {
+            var kv = new KeyValuePair<string, string>(Type.Name, ToString());
+            return JsonSerializer.Serialize(kv);
         }
     }
 }
--- src/Domain/Values/StructValue.cs
+++ src/Domain/Values/StructValue.cs
@@ -1,4 +1,6 @@
 ﻿using System.Linq;
+using System.Text;
+using System.Text.Json;
 using Elrond.Dotnet.Sdk.Domain.Exceptions;
 
 namespace Elrond.Dotnet.Sdk.Domain.Values
@@ -38,6 +40,24 @@ namespace Elrond.Dotnet.Sdk.Domain.Values
                 if (fieldType.RustType != definition.Type.RustType)
                     throw new BinaryCodecException("field rustType vs. field definitions rustType");
             }
+        }
+
+        public override string ToString()
+        {
+            var builder = new StringBuilder();
+            builder.AppendLine(Type.Name);
+            foreach (var structField in Fields)
+            {
+                builder.AppendLine($"{structField.Name}:{structField.Value}");
+            }
+
+            return builder.ToString();
+        }
+
+        public string ToJSON()
+        {
+            var dic = Fields.ToDictionary(s => s.Name, s => s.Value.ToJSON());
+            return JsonSerializer.Serialize(dic);
         }
     }
 }
